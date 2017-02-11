5c8be5d774389ec93deb261f9c9f09d8142c6a0d
Roman Smirnov <POMAHC@gmail.com>
2017-02-11 16:05:43 +0300

    Added channels support.

--- README.md
+++ README.md
@@ -23,6 +23,30 @@ config :airbrake,
 Start Airbrake process manually `Airbrake.start_link`
 or put `worker(Airbrake, [])` to your supervisors tree.
 
+
+*With Phoenix:*
+
+```elixir
+defmodule YourApp.Router do
+  use Phoenix.Router
+  use Airbrake.Plug
+
+  # ...
+end
+```
+
+in `web/web.ex`:
+```elixir
+  def channel do
+    quote do
+      use Phoenix.Channel
+      use Airbrake.Channel
+      # ...
+```
+
+
+*Custom usage example:*
+
 ```elixir
 # Report an exception.
 try do
@@ -31,3 +55,4 @@ rescue
   exception -> Airbrake.report(exception)
 end
 ```
+
---
+++ lib/airbrake/channel.ex
@@ -0,0 +1,62 @@
+defmodule Airbrake.Channel do
+  defmacro __using__(_env) do
+    quote location: :keep do
+      @before_compile Airbrake.Channel
+    end
+  end
+
+  defmacro __before_compile__(_env) do
+    quote location: :keep do
+      defoverridable [join: 3, handle_in: 3, handle_info: 2, terminate: 2]
+
+      def join(channel_name, msg, socket) do
+        try do
+          super(channel_name, msg, socket)
+        rescue
+          exception ->
+            send_to_airbrake(exception, socket.assigns, msg, %{channel: channel_name})
+        end
+      end
+
+      def handle_in(msg_type, msg, socket) do
+        try do
+          super(msg_type, msg, socket)
+        rescue
+          exception ->
+            send_to_airbrake(exception, socket.assigns, msg, %{msg_type: msg_type})
+        end
+      end
+
+      def handle_info(msg, socket) do
+        try do
+          super(msg, socket)
+        rescue
+          exception ->
+            send_to_airbrake(exception, socket.assigns, msg)
+        end
+      end
+
+      def terminate(reason, socket) do
+        try do
+          super(reason, socket)
+        rescue
+          exception ->
+            send_to_airbrake(exception, socket.assigns, %{reason: reason})
+        end
+      end
+
+      defp send_to_airbrake(exception, session, params, env \\ nil) do
+        stacktrace = System.stacktrace
+
+        Airbrake.report(exception, [
+          params: params, 
+          session: session,
+          stacktrace: stacktrace,
+          env: env
+        ])
+
+        reraise exception, stacktrace
+      end
+    end
+  end
+end

b04d04b86616142aef42aefe2b0063807ee0e191
Roman Smirnov <POMAHC@gmail.com>
2017-11-08 00:20:01 +0300

    Update Airbrake host in default config

--- config/config.exs
+++ config/config.exs
@@ -3,4 +3,4 @@ use Mix.Config
 config :airbrake,
   api_key: System.get_env("AIRBRAKE_API_KEY") || "FAKEKEY",
   project_id: System.get_env("AIRBRAKE_PROJECT_ID") || 0,
-  host: System.get_env("AIRBRAKE_HOST") || "http://collect.airbrake.io"
+  host: System.get_env("AIRBRAKE_HOST") || "https://airbrake.io"

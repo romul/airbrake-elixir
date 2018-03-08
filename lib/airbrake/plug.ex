7bcd6419498acb3498aeffd1636183037bb14e9c
Derek Kraan <derek.kraan@gmail.com>
2018-03-08 10:26:19 +0100

    Only process errors. (:throw and :exit aren't exceptions)
    From `Plug.ErrorHandler` source code:
    
    >  Once this module is used, a callback named `handle_errors/2` should
    >  be defined in your plug. This callback should accept a connection and
    >  a map
    >  containing:
    >  * the exception kind (`:throw`, `:error` or `:exit`),
    >  * the reason (an exception for errors or a term for others)
    >  * the stacktrace

--- lib/airbrake/plug.ex
+++ lib/airbrake/plug.ex
@@ -4,29 +4,33 @@ defmodule Airbrake.Plug do
     quote location: :keep do
       use Plug.ErrorHandler
 
-      defp handle_errors(conn, %{kind: _level, reason: exception, stack: stacktrace}) do
-        conn = conn |> Plug.Conn.fetch_cookies |> Plug.Conn.fetch_query_params
+      defp handle_errors(conn, %{kind: :error, reason: exception, stack: stacktrace}) do
+        conn = conn |> Plug.Conn.fetch_cookies() |> Plug.Conn.fetch_query_params()
         headers = Enum.into(conn.req_headers, %{})
 
         conn_data = %{
           url: "#{conn.scheme}://#{conn.host}:#{conn.port}#{conn.request_path}",
-          userIP: (conn.remote_ip |> Tuple.to_list() |> Enum.join(".")),
+          userIP: conn.remote_ip |> Tuple.to_list() |> Enum.join("."),
           userAgent: headers["user-agent"],
           cookies: conn.req_cookies
         }
+
         environment = %{
           headers: headers,
           httpMethod: conn.method
         }
 
-        Airbrake.Worker.remember(exception, [
+        Airbrake.Worker.remember(
+          exception,
           params: conn.params,
           session: conn.private[:plug_session],
           context: conn_data,
           env: environment,
           stacktrace: stacktrace
-        ])
+        )
       end
+
+      defp handle_errors(_conn, _map), do: nil
     end
   end
 end

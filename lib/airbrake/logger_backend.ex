b487b5aba6999101342e7e0755e1398716a5a71f
Clifton McIntosh <cmcintosh@thecitybase.com>
2020-03-29 14:56:20 -0500

    format Airbrake.LoggerBackend with "mix format"

--- lib/airbrake/logger_backend.ex
+++ lib/airbrake/logger_backend.ex
@@ -10,7 +10,6 @@ defmodule Airbrake.LoggerBackend do
     {:ok, :ok, state}
   end
 
-
   def handle_event({_level, gl, _event}, state)
       when node(gl) != node() do
     {:ok, state}
@@ -19,45 +18,52 @@ defmodule Airbrake.LoggerBackend do
   def handle_event({:error, _, {Logger, msg, _ts, _metadata}}, state) do
     try do
       err_info = parse_error_message(msg)
-      Airbrake.report(err_info[:exception], [
+
+      Airbrake.report(err_info[:exception],
         stacktrace: err_info[:stacktrace] || [],
         context: err_info[:context]
-      ])
+      )
     rescue
       exception -> IO.inspect(exception)
     end
+
     {:ok, state}
   end
+
   def handle_event(_, state) do
     {:ok, state}
   end
 
-
   defp parse_error_message(msg) do
-    msg 
+    msg
     |> to_string
     |> String.split("\n")
-    |> Enum.reverse
+    |> Enum.reverse()
     |> Enum.reduce(%{stacktrace: [], context: %{}}, &parse_line/2)
   end
 
   defp parse_line("    " <> line, res), do: parse_line(line, res)
-  defp parse_line("** "  <> err_msg, res) do
+
+  defp parse_line("** " <> err_msg, res) do
     case Regex.run(~r/\((.*?)\)\s*(.*?)\z/, err_msg) do
       [_, type, message] ->
-        Map.put_new(res, :exception, [type: type, message: message])
+        Map.put_new(res, :exception, type: type, message: message)
+
       _ ->
-        Map.put_new(res, :exception, [type: "RuntimeError", message: err_msg])
+        Map.put_new(res, :exception, type: "RuntimeError", message: err_msg)
     end
   end
+
   defp parse_line("(" <> _ = st_line, res) do
     Map.put(res, :stacktrace, [st_line | Map.get(res, :stacktrace, [])])
   end
+
   defp parse_line(line, res) do
     case String.split(line, ": ", parts: 2) do
       [key, value] ->
-        key = key |> String.downcase |> String.to_atom
+        key = key |> String.downcase() |> String.to_atom()
         put_in(res, [:context, key], value)
+
       [value] ->
         put_in(res, [:context, :title], value)
     end
